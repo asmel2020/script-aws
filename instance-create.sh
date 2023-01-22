af467fc2164e64c4e6c0e4c62639e8116f49852f
Danny Gonzalez <danny.galicia@rhizom.me>
2023-01-22 07:17:03 -0300

    feat:

--- instance-create.sh
+++ instance-create.sh
@@ -1,3 +1,3 @@
 amazon_linux_2
 
-aws lightsail create-instances --instance-names Instance-1 --availability-zone us-east-1a --blueprint-id amazon_linux_2 --bundle-id nano_2_0 --user-data file://docker-install.sh
+aws lightsail create-instances --instance-names Instance-1 --availability-zone us-east-1a --blueprint-id amazon_linux_2 --bundle-id small_2_0 --user-data file://docker-install.sh
